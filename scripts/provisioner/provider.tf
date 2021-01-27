3d07c000813d3b89064b1e70b131125ae684611b
SreeramPrabhakar <77006896+SreeramPrabhakar@users.noreply.github.com>
2021-01-27 12:12:55 +0530

    Update provider.tf

--- scripts/provisioner/provider.tf
+++ scripts/provisioner/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIATHF6Z5VDQXSVLM6Shello"
-    secret_key = "zI61XgdyF8Hm1e1g7QNOLkqywXti9oHHeZAQ4E/phello"
+    access_key = "<accesskey>"
+    secret_key = "<secretkey>"
 }
